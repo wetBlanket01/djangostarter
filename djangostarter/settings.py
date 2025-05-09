0ca28b7c24f57af03dd08f537625dccc1b40e98c
wetBlanket01 <zhenyazao06@gmail.com>
2025-05-09 13:12:44 +0300

    Loaded Favicon. Change in settings

--- djangostarter/settings.py
+++ djangostarter/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wn9y#c1m=b0pdf12ohppbhgcg94=&u*h)hdgyy=zc#b)ymjrtd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 
